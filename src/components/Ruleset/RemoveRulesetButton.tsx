e6af89beef9bb83d1e64505c9c2c8922f6c4945b
Radim Kotajny <67335383+RadimKotajnyDev@users.noreply.github.com>
2023-07-12 11:25:47 +0200

    added priority buttons, working on their functions + fixed padding of RemoveRulesetButton

--- src/components/Ruleset/RemoveRulesetButton.tsx
+++ src/components/Ruleset/RemoveRulesetButton.tsx
@@ -4,7 +4,7 @@ import Button from "../elements/Button.tsx";
 export const RemoveRulesetButton = (props: any) => {
   return (
     <Button onClickProp={props.removeRuleset}
-            classNameProp="ButtonClass bg-red-600"
+            classNameProp="ButtonClass bg-red-600 p-3"
     >
       <AiOutlineClose size={25}/>
     </Button>
