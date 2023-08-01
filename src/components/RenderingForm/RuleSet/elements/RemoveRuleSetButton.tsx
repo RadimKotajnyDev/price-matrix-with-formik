599fd3b2d5ef08a281a4b0b89dcde1c08001a0d6
Radim Kotajny <67335383+RadimKotajnyDev@users.noreply.github.com>
2023-08-01 17:16:32 +0200

    improved css sizes

--- src/components/RenderingForm/RuleSet/elements/RemoveRuleSetButton.tsx
+++ src/components/RenderingForm/RuleSet/elements/RemoveRuleSetButton.tsx
@@ -3,7 +3,7 @@ import {AiOutlineDelete} from "react-icons/ai";
 export const RemoveRuleSetButton = (props: any) => {
   return (
     <button onClick={props.removeRuleSet} type="button"
-            className="ButtonClass flex flex-row items-center justify-center gap-2 bg-red-600 py-2 px-[10px]"
+            className="ButtonClass text-sm flex flex-row items-center justify-center gap-1 bg-red-600 py-1 px-2"
     >
       <AiOutlineDelete size={20}/> Ruleset
     </button>
