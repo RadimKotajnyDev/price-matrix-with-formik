b2273bb4f5000b766573074aac701a3a82de51ce
Radim Kotajny <67335383+RadimKotajnyDev@users.noreply.github.com>
2023-08-02 08:57:55 +0200

    fixed label of note (there was offercode instead of note)

--- src/components/RenderingForm/RuleSet/elements/Note.tsx
+++ src/components/RenderingForm/RuleSet/elements/Note.tsx
@@ -3,7 +3,7 @@ import {Field} from "formik";
 export default function Note(props: {nameProp: string}) {
   return (
     <div className="flex flex-col pb-5 border-b-2 border-gray-200 w-full">
-      <label className="LabelClass">Offer Code</label>
+      <label className="LabelClass">Note</label>
       <Field type="text" name={props.nameProp} className="InputClass" />
     </div>
   )
