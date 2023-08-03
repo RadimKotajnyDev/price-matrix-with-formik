fc2f2805cee01dffb2cce4ee6011f2fb1bc4a3f3
Radim Kotajny <67335383+RadimKotajnyDev@users.noreply.github.com>
2023-08-03 15:59:18 +0200

    Top-level-await build error fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: ['chrome89', 'edge89', 'firefox89', 'opera75', 'safari15'] // top-level-await fix
+  },
   plugins: [react()],
   server: {
     host: false
