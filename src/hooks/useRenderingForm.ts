d111a166985da47f5d8b59041fabfdd6d0c80744
Adam Ko <96392772+adamko06@users.noreply.github.com>
2023-08-11 15:02:18 +0200

    Update useRenderingForm.ts

--- src/hooks/useRenderingForm.ts
+++ src/hooks/useRenderingForm.ts
@@ -2,14 +2,14 @@ import {useEffect, useRef, useState} from "react";
 import {FetchData, ReformatRuleSets} from "../configs/API.tsx";
 import {ScrollToTop} from "../components/RenderingForm/functions/RenderFunctions.ts";
 
-export function useRenderingForm() {
+export function useRenderingForm() { // TODO: use arrow function
 
   const [resolvedRuleSets, setResolvedRuleSets] = useState([])
   const [matrixData, setMatrixData] = useState({name: "", id: 0})
 
-  const [loadingSpin, setLoadingSpin] = useState<boolean>(true);
+  const [loadingSpin, setLoadingSpin] = useState<boolean>(true); //TODO: all useState should be sorted at the top for better readability, (isLoadingSpinner)
   useEffect(() => {
-    FetchData().then((res) => setMatrixData(res))
+    FetchData().then((res) => setMatrixData(res)) //TODO: change this for async / await -> newer and better readability
     ReformatRuleSets()
       .then((res) => {
         setResolvedRuleSets(res);
@@ -17,12 +17,12 @@ export function useRenderingForm() {
       })
   }, [loadingSpin])
 
-  const RefOnTop = useRef(null)
+  const RefOnTop = useRef(null) // TODO: change to camelCase, better nam
 
-  const [lastRuleSetAdded, setLastRuleSetAdded] = useState<boolean>(false);
-  const [ruleSetToRemoveAnimation, setRuleSetToRemoveAnimation] = useState<number | null>(null);
+  const [lastRuleSetAdded, setLastRuleSetAdded] = useState<boolean>(false); // TODO: "islastRuleSetAdded"
+  const [ruleSetToRemoveAnimation, setRuleSetToRemoveAnimation] = useState<number | null>(null); // TODO: better name, something with index
 
-  const AddRulesetAnimate = () => {
+  const AddRulesetAnimate = () => { // TODO: all functions change to camelCase (and all other for consistent), look at the other names, more concise and to the point
     setLastRuleSetAdded(true);
     setTimeout(() => {
       setLastRuleSetAdded(false);
@@ -30,8 +30,8 @@ export function useRenderingForm() {
     }, 500);
   };
 
-  const [requestModalState, setRequestModalState] = useState<boolean>(false)
-  const [ErrorStateModal, setErrorStateModal] = useState<boolean>(false)
+  const [requestModalState, setRequestModalState] = useState<boolean>(false) // TODO isRequestModal
+  const [ErrorStateModal, setErrorStateModal] = useState<boolean>(false) // TODO: camelCase, isErrorModal
   useEffect(() => {
     if (requestModalState) {
       const timeout = setTimeout(() => {
@@ -41,7 +41,7 @@ export function useRenderingForm() {
     }
   }, [requestModalState]);
 
-  function DisplayError() {
+  function DisplayError() { // TODO: use arow function here to be consistent
     setErrorStateModal(true)
     setRequestModalState(true)
   }
