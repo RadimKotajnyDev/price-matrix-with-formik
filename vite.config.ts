842a744690aae68df6f55a248a74dbc441d9f592
mole06 <adam.kozusnik@seznam.cz>
2023-08-16 12:01:07 +0200

    edit inputs and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  /*build: {
-    target: ['chrome89', 'edge89', 'firefox89', 'opera75', 'safari15'] // top-level-await fix
-  },*/
   plugins: [react()],
+  base: './',
   server: {
     host: false
-  }
-
+  },
 })
